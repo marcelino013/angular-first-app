22c3bef34da42ef9f75d5cae38348b5cf439b311
Marcelino Parado <marcelino.parado@gmail.com>
2023-12-16 22:00:28 +0800

    before updating to use service components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app-2';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
